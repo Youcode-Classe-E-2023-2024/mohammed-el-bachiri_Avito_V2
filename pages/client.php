6531f82db9fc225f75d5268145034d504281378a
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-22 10:59:35 +0100

    create client page

---
+++ pages/client.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Client Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+
+<!-- Navbar -->
+<nav class="bg-orange-500 p-4 text-white">
+    <div class="container mx-auto">
+        <h1 class="text-2xl font-bold">Client Dashboard</h1>
+    </div>
+</nav>
+
+<div class="container mx-auto p-8">
+
+    <!-- Client Content -->
+    <h2 class="text-3xl font-bold mb-4">Available Products</h2>
+
+    <!-- List of Available Products -->
+    <ul>
+        <!-- Replace the following with actual product data -->
+        <li>Product A <button class="bg-green-500 text-white px-2 py-1">Buy</button></li>
+        <li>Product B <button class="bg-green-500 text-white px-2 py-1">Buy</button></li>
+        <!-- Add more products as needed -->
+    </ul>
+
+</div>
+
+</body>
+</html>
