4c42f57793a774eee65fa8b99d6eae2c54e5db46
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-22 10:59:10 +0100

    create seller page

---
+++ pages/seller.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seller Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+
+<!-- Navbar -->
+<nav class="bg-green-500 p-4 text-white">
+    <div class="container mx-auto">
+        <h1 class="text-2xl font-bold">Seller Dashboard</h1>
+    </div>
+</nav>
+
+<div class="container mx-auto p-8">
+
+    <!-- Seller Content -->
+    <h2 class="text-3xl font-bold mb-4">Your Products</h2>
+
+    <!-- List of Seller's Products -->
+    <ul>
+        <!-- Replace the following with actual product data -->
+        <li>Product 1 <button class="bg-blue-500 text-white px-2 py-1">Edit</button> <button class="bg-red-500 text-white px-2 py-1">Delete</button></li>
+        <li>Product 2 <button class="bg-blue-500 text-white px-2 py-1">Edit</button> <button class="bg-red-500 text-white px-2 py-1">Delete</button></li>
+        <!-- Add more products as needed -->
+    </ul>
+
+</div>
+
+</body>
+</html>
