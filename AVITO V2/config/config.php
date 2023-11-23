e58c28cd8e1ac4580611141e96c807a429094b61
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-23 11:03:35 +0100

    create user table

--- AVITO V2/config/config.php
+++ AVITO V2/config/config.php
@@ -10,4 +10,19 @@ if($connect == false){
     die("Could not connect to Database !  . $connect->connect_errno");
 }
 
+$sql = "CREATE TABLE IF NOT EXISTS user(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            first_name VARCHAR(50),
+            last_name VARCHAR(50),
+            email VARCHAR(250),
+            password VARCHAR(250)
+    )";
+
+$result = $connect->query($sql);
+
+if(!$result){
+    die("could not create table ! . $connect->errno");
+
+}
+
 ?>
