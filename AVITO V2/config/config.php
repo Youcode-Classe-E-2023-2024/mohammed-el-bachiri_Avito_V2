26a57c7f23c6927eec8b7350573bd77aa02787a0
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-23 10:28:44 +0100

    connect to database

--- AVITO V2/config/config.php
+++ AVITO V2/config/config.php
@@ -1,1 +1,13 @@
 <?php
+define("db_host", "localhost");
+define("db_user", "root");
+define("db_pass", "");
+define("db_name", "avito_v2");
+
+$connect = mysqli_connect(db_host, db_user, db_pass, db_name);
+
+if($connect == false){
+    die("Could not connect to Database !  . $connect->connect_errno");
+}
+
+?>
