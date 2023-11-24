adc40febe2f2ae866777e763062b789f47693677
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-24 16:12:36 +0100

    check if email exist and send session

--- AVITO V2/include/signup_traitment.php
+++ AVITO V2/include/signup_traitment.php
@@ -1,18 +1,26 @@
 <?php
+session_start();
 include '../config/config.php';
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $email = $_POST['email'];
     $pass = $_POST['pass'];
+
 if (isset($_POST['seller_btn'])) {
-    $sql = "INSERT INTO users (first_name, last_name, email, password, role) VALUES (?, ?, ?, ?, 'seller')";
-    $stmt =  $connect->prepare($sql);
-    $stmt->bind_param('ssss', $fname, $lname, $email, $pass);
-    $result = $stmt->execute();
-    if ($result == true) {
-        header('location: ../login.php');
+
+    $sql_select = "SELECT email FROM users WHERE email = ?";
+    $stmt1 = $connect->prepare($sql_select);
+    $stmt1->bind_param('s', $email);
+    $result1 = $stmt1->execute();
+    $arr = $stmt1->get_result();
+
+    if ($arr->num_rows != 0) {
+        header('location: ../signup.php');
+        $_SESSION['acc_exist'] = true;
     } else {
         echo 'error : ' . mysqli_error($connect);
     }
 }
 
+exit();
+?>
