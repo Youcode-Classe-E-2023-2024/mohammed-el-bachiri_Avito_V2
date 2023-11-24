e004cd29ce9d8214b0c855e10f8e094a0592ead0
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-24 13:29:51 +0100

    Update signup_traitment.php

--- AVITO V2/include/signup_traitment.php
+++ AVITO V2/include/signup_traitment.php
@@ -1,1 +1,18 @@
 <?php
+include '../config/config.php';
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+if (isset($_POST['seller_btn'])) {
+    $sql = "INSERT INTO users (first_name, last_name, email, password, role) VALUES (?, ?, ?, ?, 'seller')";
+    $stmt =  $connect->prepare($sql);
+    $stmt->bind_param('ssss', $fname, $lname, $email, $pass);
+    $result = $stmt->execute();
+    if ($result == true) {
+        header('location: ../login.php');
+    } else {
+        echo 'error : ' . mysqli_error($connect);
+    }
+}
+
