22ac4974219eb1af537432a0c2ec54df6d660a04
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-24 10:32:49 +0100

    add two user and products tables

--- AVITO V2/config/config.php
+++ AVITO V2/config/config.php
@@ -7,16 +7,25 @@ define("db_name", "avito_v2");
 $connect = mysqli_connect(db_host, db_user, db_pass, db_name);
 
 if($connect == false){
-    die("Could not connect to Database !  . $connect->connect_errno");
+    die("Could not connect to Database !"  . mysqli_connect_error());
 }
+$sql1 = "
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+); ";
 
-$sql = "CREATE TABLE IF NOT EXISTS user(
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            first_name VARCHAR(50),
-            last_name VARCHAR(50),
-            email VARCHAR(250),
-            password VARCHAR(250)
-    )";
+$sql2 = "
+CREATE TABLE Products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price INT NOT NULL
+);";
 
 $result = $connect->query($sql);
 
