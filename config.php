a0d532dc70eb4c74a11d0d081cd469e6695e3d5e
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-27 09:15:08 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php
+define("db_host", "localhost");
+define("db_user", "root");
+define("db_pass", "");
+define("db_name", "avito_v2");
+
+$connect = mysqli_connect(db_host, db_user, db_pass, db_name);
+
+if($connect == false){
+    die("Could not connect to Database !"  . mysqli_connect_error());
+}
+$sql1 = "
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+); ";
+
+$sql2 = "
+    CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL NOT NULL,
+    photo LONGBLOB NOT NULL,
+    user_id INT
+);";
+
+$rsl1 = $connect->query($sql1);
+$rsl2 = $connect->query($sql2);
+if (!$rsl1 || !$rsl2) {
+    die("could not create table ! . $connect->errno");
+}
+
+?>
