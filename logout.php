c823a94112b6b9b93fa5691640d7565dff67c8fb
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-27 09:15:22 +0100

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+if (isset($_POST['seller_logout_btn'])) {
+    unset($_SESSION['seller_logged']);
+    header('location: login.php');
+}
+if (isset($_POST['client_logout_btn'])) {
+    unset($_SESSION['client_logged']);
+    header('location: login.php');
+}
+if (isset($_POST['admin_logout_btn'])) {
+    unset($_SESSION['admin_logged']);
+    header('location: login.php');
+}
+exit();
+?>
